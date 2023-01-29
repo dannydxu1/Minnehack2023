e009f892c6992677abccc306650a8f29ba545c3f
gujje002 <gujje002@umn.edu>
2023-01-29 10:07:05 -0600

    Create google_civic.js

---
+++ google_civic.js
@@ -0,0 +1,7 @@
+/* Initialize the API client and make a request.
+*/
+function load() {
+    gapi.client.setApiKey('AIzaSyCRhHS2SXK0FmW46cK8Xo78b8bEs364VBc');
+    lookup('', renderResults);
+    }
+
