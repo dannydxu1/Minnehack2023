2bfaca1e21d8ae397da2b6c68a5d6be9f9e3d9e6
Danny Xu <danny.d.xu@gmail.com>
2023-01-29 10:04:35 -0600

    pushing before I merge all the functions and implement NLP

--- ballotScraper.js
+++ ballotScraper.js
@@ -1,7 +1,7 @@
-const cheerio = require('cheerio'); 
-const axios = require("axios")
+const cheerio = require("cheerio");
+const axios = require("axios");
 const stats = [];
-const profits = []; 
+const profits = [];
 const breakdown = [];
 var testsPassed = 0;
 var pe = 0;
@@ -9,148 +9,147 @@ var eps = 0;
 var ass = 0;
 var lia = 0;
 
-
 async function getInfo(testURL) {
-  axios.get(testURL).then(urlResponse =>  {
+  axios.get(testURL).then((urlResponse) => {
     const $ = cheerio.load(urlResponse.data);
-
-
   });
-
 }
 
 async function getSummary(urlSum) {
-    axios.get(urlSum).then(urlResponse =>  {
-       
-        const $ = cheerio.load(urlResponse.data);
-    
-        $("tr.Bxz\\(bb\\)").each((i, element) => {
-            const link = $(element)
-            .find("td.Ta\\(end\\)")
-            .text()
-            stats.push(link)
-            console.log(link)
-        });
-    //if bored in future, find a way to find using data-test attribute
-        for (let i = 0; i < stats.length; i++) {
-            if( i==10)  {
-                pe = stats[i]
-            console.log("PE RATIO(TTM):" + stats[i]);   
-            }
-            if( i==11)  {
-                eps = stats[i]
-            console.log("EPS(TTM): " + stats[i]);   
-            }
-        }
-    });
-    
-}
+  axios.get(urlSum).then((urlResponse) => {
+    const $ = cheerio.load(urlResponse.data);
 
+    $("tr.Bxz\\(bb\\)").each((i, element) => {
+      const link = $(element).find("td.Ta\\(end\\)").text();
+      stats.push(link);
+      console.log(link);
+    });
+    //if bored in future, find a way to find using data-test attribute
+    for (let i = 0; i < stats.length; i++) {
+      if (i == 10) {
+        pe = stats[i];
+        console.log("PE RATIO(TTM):" + stats[i]);
+      }
+      if (i == 11) {
+        eps = stats[i];
+        console.log("EPS(TTM): " + stats[i]);
+      }
+    }
+  });
+}
 
 async function getFinancials(urlFin) {
-    axios.get(urlFin).then(urlResponse =>  {
+  axios.get(urlFin).then((urlResponse) => {
+    const $ = cheerio.load(urlResponse.data);
 
-    
-        const $ = cheerio.load(urlResponse.data);
-    
-        $("div.Ta\\(c\\).Py\\(6px\\).Bxz\\(bb\\).BdB").each((i, element) => {
-            const link = $(element)
-            .find("span")
-            .text()
-            profits.push(link)
-          //  console.log(link)
-        });
-    
-        for (let i = 1; i < 4; i++) {
-            console.log(profits[i]+": "+profits[14+i]);
-        }
-       
-        
+    $("div.Ta\\(c\\).Py\\(6px\\).Bxz\\(bb\\).BdB").each((i, element) => {
+      const link = $(element).find("span").text();
+      profits.push(link);
+      //  console.log(link)
     });
+
+    for (let i = 1; i < 4; i++) {
+      console.log(profits[i] + ": " + profits[14 + i]);
+    }
+  });
 }
 
-async function getBal(urlBal) { 
-    axios.get(urlBal).then(urlResponse =>  {
-    
-        const $ = cheerio.load(urlResponse.data);
-    
-        $("div.Ta\\(c\\).Py\\(6px\\).Bxz\\(bb\\).BdB").each((i, element) => {
-            const link = $(element)
-            .find("span")
-            .text()
-            breakdown.push(link)
-          //  console.log(link)
-        });
-        for (let i = 0; i < breakdown.length; i++) {
-            if( i==4)  {
-            ass = breakdown[i];
-            console.log("TOTAL ASSETS: " + breakdown[i]);   
-            }
-            if( i==8)  {
-            lia = breakdown[i];
-            console.log("TOTAL LIABILITIES: " + breakdown[i]);   
-            }
-        }
-        
+async function getBal(urlBal) {
+  axios.get(urlBal).then((urlResponse) => {
+    const $ = cheerio.load(urlResponse.data);
+
+    $("div.Ta\\(c\\).Py\\(6px\\).Bxz\\(bb\\).BdB").each((i, element) => {
+      const link = $(element).find("span").text();
+      breakdown.push(link);
+      //  console.log(link)
     });
+    for (let i = 0; i < breakdown.length; i++) {
+      if (i == 4) {
+        ass = breakdown[i];
+        console.log("TOTAL ASSETS: " + breakdown[i]);
+      }
+      if (i == 8) {
+        lia = breakdown[i];
+        console.log("TOTAL LIABILITIES: " + breakdown[i]);
+      }
+    }
+  });
 }
 
 async function getPassed() {
-    let year1 = profits[15].replace(/,/g,'') 
-    let year2= profits[16].replace(/,/g,'') 
-    let year3 = profits[17].replace(/,/g,'') 
+  let year1 = profits[15].replace(/,/g, "");
+  let year2 = profits[16].replace(/,/g, "");
+  let year3 = profits[17].replace(/,/g, "");
 
-    //tests PE ratio
-    if (pe < 25 ) {
-        testsPassed++;
-        console.log("CRITERION [1] MET: PE under 25")
-    } else {
-        console.log("CRITERION [1] FAILED: PE under 25")
-    }
+  //tests PE ratio
+  if (pe < 25) {
+    testsPassed++;
+    console.log("CRITERION [1] MET: PE under 25");
+  } else {
+    console.log("CRITERION [1] FAILED: PE under 25");
+  }
 
-    //tests Earnings per share
-    if (eps > 2) {
-        testsPassed++;
-        console.log("CRITERION [2] MET: EPS over 2")
-    } else {
-        console.log("CRITERION [2] FAILED: EPS over 2")
-    }
+  //tests Earnings per share
+  if (eps > 2) {
+    testsPassed++;
+    console.log("CRITERION [2] MET: EPS over 2");
+  } else {
+    console.log("CRITERION [2] FAILED: EPS over 2");
+  }
 
-    //tests 3 years of net profit
-    if (year1 > 0  && year2 > 0 && year3 > 0) {
-        testsPassed++;
-        console.log("CRITERION [3] MET: 3 YEARS OF NET PROFIT")
-    } else {
-        console.log("CRITERION [3] FAILED: 3 YEARS OF NET PROFIT")
-    }
+  //tests 3 years of net profit
+  if (year1 > 0 && year2 > 0 && year3 > 0) {
+    testsPassed++;
+    console.log("CRITERION [3] MET: 3 YEARS OF NET PROFIT");
+  } else {
+    console.log("CRITERION [3] FAILED: 3 YEARS OF NET PROFIT");
+  }
 
-    //test Asset to Liability ratio
-    ass = ass.replace(/,/g,'') 
-    lia = lia.replace(/,/g,'') 
-    let ratio = ass/lia
-    if (ass/lia > 2 ) {
-        testsPassed++;
-        console.log("CRITERION [4] MET: ASSET:LIABILITY RATIO IS 2:1 OR BETTER (ratio is "+ratio+")")
-    } else {
-        console.log("CRITERION [4] FAILED: ASSET:LIABILITY RATIO IS NOT 2:1 OR BETTER(ratio is "+ratio+")")
-    }
-    if (testsPassed>=3) { 
-        console.log("I would recommend this stock.")
-    } else {
-        console.log("I would NOT recommend this stock.")
-    }
+  //test Asset to Liability ratio
+  ass = ass.replace(/,/g, "");
+  lia = lia.replace(/,/g, "");
+  let ratio = ass / lia;
+  if (ass / lia > 2) {
+    testsPassed++;
+    console.log(
+      "CRITERION [4] MET: ASSET:LIABILITY RATIO IS 2:1 OR BETTER (ratio is " +
+        ratio +
+        ")"
+    );
+  } else {
+    console.log(
+      "CRITERION [4] FAILED: ASSET:LIABILITY RATIO IS NOT 2:1 OR BETTER(ratio is " +
+        ratio +
+        ")"
+    );
+  }
+  if (testsPassed >= 3) {
+    console.log("I would recommend this stock.");
+  } else {
+    console.log("I would NOT recommend this stock.");
+  }
 }
 async function getAdvice(TICKER) {
   //  console.log(testsPassed)
-     console.log("*****")
-    await getSummary("https://finance.yahoo.com/quote/"+TICKER+"?p="+TICKER+"&.tsrc=fin-srch")
-    await  getFinancials("https://finance.yahoo.com/quote/"+TICKER+"/financials?p="+TICKER)
-    await getBal("https://finance.yahoo.com/quote/"+TICKER+"/balance-sheet?p="+TICKER) 
-    //very dumb solution
-    setTimeout(() => {
-        console.log("*****")
-        getPassed()
-    }, 2000); 
+  console.log("*****");
+  await getSummary(
+    "https://finance.yahoo.com/quote/" +
+      TICKER +
+      "?p=" +
+      TICKER +
+      "&.tsrc=fin-srch"
+  );
+  await getFinancials(
+    "https://finance.yahoo.com/quote/" + TICKER + "/financials?p=" + TICKER
+  );
+  await getBal(
+    "https://finance.yahoo.com/quote/" + TICKER + "/balance-sheet?p=" + TICKER
+  );
+  //very dumb solution
+  setTimeout(() => {
+    console.log("*****");
+    getPassed();
+  }, 2000);
 }
 
-getAdvice("AAPL")
+getAdvice("AAPL");
--- index.js
+++ index.js
@@ -1,13 +1,69 @@
-const request = require('request-promise');
-const cheerio = require('cheerio');
+const request = require("request-promise");
+const cheerio = require("cheerio");
+const fs = require("fs");
 //https://medium.com/@stefanhyltoft/scraping-html-tables-with-nodejs-request-and-cheerio-e3c6334f661b
 
+// scrape candidates
 async function main() {
- const result = await request.get("https://ballotpedia.org/Tim_Walz");
- const $ = cheerio.load(result);
- $("#mw-content-text > div > p:nth-child(11)").each((index, element) => {
-   console.log($(element).text());
- });
+  console.log(
+    "Now printing the candidates from the Minnesota 2022 Governor Election"
+  );
+  console.log();
+  const govenors = [
+    "Tim_Walz",
+    "Scott_Jensen_(Minnesota)",
+    "Steve_Patterson",
+    "Hugh_McTavish",
+    "James_McCaskel",
+    "Gabrielle_Prosser",
+  ];
+  const selectors = [
+    "#mw-content-text > div > p:nth-child(11)",
+    "#mw-content-text > div > p:nth-child(9)",
+    "#mw-content-text > div > p:nth-child(9)",
+    "#mw-content-text > div > p:nth-child(10)",
+    "#mw-content-text > div > p:nth-child(9)",
+    "#mw-content-text > div > p:nth-child(9)",
+  ];
+  const allInfo = [];
+  for (let i = 0; i < govenors.length; i++) {
+    var link = "https://ballotpedia.org/" + govenors[i];
+    await console.log(link);
+    await getCandidateInfo(link, selectors[i]);
+  }
 }
+
+/** find political party function  */
+async function getCandidateInfo(link, selector) {   
+    const susArr = [];
+  const bioArr = [];
+  const infoArr = [];
+  const result = await request.get(link);
+  const $ = cheerio.load(result);
+  $("#mw-content-text > div > p:nth-child(6)").each((index, element) => {
+    const basicData = $(element).text().replace(/\s+/g, " ").trim();
+    console.log(basicData);
+    bioArr.push(basicData);
+    susArr.push(basicData)
+  });
+  $(selector).each((index, element) => {
+    const bioData = $(element).text();
+    console.log(bioData);
+    bioArr.push(bioData);
+    susArr.push(bioData)
+  });
+
  
+}
+
 main();
+
+/*
+ await fs.writeFile("candidateData.json", JSON.stringify(bioArr, null, 2), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("Data written to file successfully!");
+  });
+*/
